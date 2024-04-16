e1644e6af92eeaa78ada432c229ea61253d3e28b
meghnayork <meghnap@york.ie>
2024-04-16 12:30:36 +0530

    static cart

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -14,7 +14,45 @@ const Cart = () => {
   const [checkout, setCheckout] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [relevantProduct, setRelevantProduct] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
+  const [totalPrice, setTotalPrice] = useState(50);
+  const [staticProduct, setStaticProduct] = useState([
+    {
+      productId: 1,
+      title: "Product 1",
+      desc: "Variant",
+      price: 22,
+    },
+  ]);
+  const [staticRel, setStaticRel] = useState([
+    {
+      productId: 101,
+      title: "Product 101",
+      desc: "Variant",
+      price: 20,
+      addBtn: "Add to My Order",
+    },
+    {
+      productId: 102,
+      title: "Product 102",
+      desc: "Variant",
+      price: 22,
+      addBtn: "Add to My Order",
+    },
+    {
+      productId: 103,
+      title: "Product 103",
+      desc: "Variant",
+      price: 22,
+      addBtn: "Add to My Order",
+    },
+    {
+      productId: 104,
+      title: "Product 104",
+      desc: "Variant",
+      price: 22,
+      addBtn: "Add to My Order",
+    },
+  ]);
 
   // get domain and product :: start
   useEffect(() => {
@@ -89,16 +127,20 @@ const Cart = () => {
 
   // cart update :: start
   useEffect(() => {
-    const total = filteredProducts.reduce(
-      (acc, product) => acc + product.quantity * product.variants[0].price,
-      0
-    );
-    setTotalPrice(total);
+    if (filteredProducts.length > 0) {
+      const total = filteredProducts.reduce(
+        (acc, product) => acc + product.quantity * product.variants[0].price,
+        0
+      );
+      setTotalPrice(total);
 
-    generateCartLink();
+      generateCartLink();
+    } else {
+      setTotalPrice(50);
+      setCheckout("");
+    }
   }, [filteredProducts]);
   // cart update :: end
-
   // remove product from cart :: start
   const removeProduct = (productIdToRemove) => {
     setFilteredProducts(
@@ -116,6 +158,24 @@ const Cart = () => {
         prevProducts.map((product) =>
           product.variants[0].id === productIdToRemove
             ? { ...product, addBtn: addBtn }
+            : product
+        )
+      );
+    }
+  };
+
+  const deleteProduct = (id) => {
+    setStaticProduct(staticProduct.filter((pro) => pro.productId !== id));
+
+    const removedProduct = staticRel.find(
+      (product) => product.productId === id
+    );
+
+    if (removedProduct) {
+      setStaticRel((pro) =>
+        pro.map((product) =>
+          product.productId === id
+            ? { ...product, addBtn: "Add to My Order" }
             : product
         )
       );
@@ -156,6 +216,19 @@ const Cart = () => {
       );
     }
   };
+
+  const staticAddToCart = (id) => {
+    const productToAdd = staticRel.find((product) => product.productId === id);
+    if (productToAdd) {
+      setStaticProduct([...staticProduct, { ...productToAdd, quantity: 1 }]);
+
+      setStaticRel((prevProducts) =>
+        prevProducts.map((product) =>
+          product.productId === id ? { ...product, addBtn: "Added!" } : product
+        )
+      );
+    }
+  };
   // add from relevant products :: end
 
   // generate cart link :: start
@@ -180,72 +253,79 @@ const Cart = () => {
       />
       {/* header :: end */}
 
-      {domain && productId && (
-        <>
-          {/* announcement bar :: start */}
-          <AnnouncementBar
-            className={"bg-mid-blue"}
-            position={"relative"}
-            content={`We’ve reserved your order! <br /> Checkout within <b>9:38</b> to guarantee your order.`}
-          />
-          {/* announcement bar :: end */}
-        </>
-      )}
+      {/* announcement bar :: start */}
+      <AnnouncementBar
+        className={"bg-mid-blue"}
+        position={"relative"}
+        content={`We’ve reserved your order! <br /> Checkout within <b>9:38</b> to guarantee your order.`}
+      />
+      {/* announcement bar :: end */}
 
       <div className="body-content">
-        {domain && productId ? (
-          <>
-            {/* progressbar :: start */}
-            <ProgressBar addedAmout={totalPrice} totalAmout={200} />
-            {/* progressbar :: end */}
+        <>
+          {/* progressbar :: start */}
+          <ProgressBar addedAmout={totalPrice} totalAmout={200} />
+          {/* progressbar :: end */}
 
-            {/* product card :: start */}
-            <div className="cart-item-wrap">
-              <h3 className="section-title">Your Cart</h3>
-              {filteredProducts.map((product) => (
-                <HorizontalProductCard
-                  productImg={product.variants[0].featured_image.src}
-                  title={product.title}
-                  desc={product.variants[0].title}
-                  price={product.variants[0].price}
-                  qty={product.quantity}
-                  updateQuantity={(newQuantity) =>
-                    updateQuantity(product.variants[0].id, newQuantity)
-                  }
-                  remove={() => removeProduct(product.variants[0].id)}
-                  totalPrice={(data) => console.log(data)}
-                />
-              ))}
-            </div>
+          {/* product card :: start */}
+          <div className="cart-item-wrap">
+            <h3 className="section-title">Your Cart</h3>
+            {filteredProducts.length > 0
+              ? filteredProducts.map((product) => (
+                  <HorizontalProductCard
+                    productImg={product.variants[0].featured_image.src}
+                    title={product.title}
+                    desc={product.variants[0].title}
+                    price={product.variants[0].price}
+                    qty={product.quantity}
+                    updateQuantity={(newQuantity) =>
+                      updateQuantity(product.variants[0].id, newQuantity)
+                    }
+                    remove={() => removeProduct(product.variants[0].id)}
+                    totalPrice={(data) => console.log(data)}
+                  />
+                ))
+              : staticProduct.map((product) => (
+                  <HorizontalProductCard
+                    title={product.title}
+                    desc={product.desc}
+                    price={product.price}
+                    totalPrice={(data) => console.log(data)}
+                    updateQuantity={(newQuantity) => console.log(newQuantity)}
+                    remove={() => deleteProduct(product.productId)}
+                  />
+                ))}
+          </div>
 
-            <h3 className="section-title">You May Also Like</h3>
-            <div className="relevant-product-wrap pb-extra-50">
-              {relevantProduct.map((product) => (
-                <RelevantProductCard
-                  img={product.variants[0].featured_image.src}
-                  title={product.title}
-                  amount={`$${product.variants[0].price}`}
-                  discount={"-20%"}
-                  addToCart={() => addToCart(product.variants[0].id)}
-                  btnText={product.addBtn || addBtn}
-                />
-              ))}
-            </div>
-            {/* product card :: end */}
-          </>
-        ) : (
-          <>
-            {/* announcement bar :: start */}
-            <h3 className="text-center text-dark">No products added in cart</h3>
-            {/* announcement bar :: end */}
-          </>
-        )}
+          <h3 className="section-title">You May Also Like</h3>
+          <div className="relevant-product-wrap pb-extra-50">
+            {relevantProduct.length > 0
+              ? relevantProduct.map((product) => (
+                  <RelevantProductCard
+                    img={product.variants[0].featured_image.src}
+                    title={product.title}
+                    amount={`$${product.variants[0].price}`}
+                    discount={"-20%"}
+                    addToCart={() => addToCart(product.variants[0].id)}
+                    btnText={product.addBtn || addBtn}
+                  />
+                ))
+              : staticRel.map((product) => (
+                  <RelevantProductCard
+                    title={product.title}
+                    amount={`$${product.price}`}
+                    discount={"-20%"}
+                    addToCart={() => staticAddToCart(product.productId)}
+                    btnText={product.addBtn}
+                  />
+                ))}
+          </div>
+          {/* product card :: end */}
+        </>
       </div>
 
       {/* cta banner :: start */}
-      {domain && productId && (
-        <CTABanner ctaContent={"Checkout Now"} link={checkout} />
-      )}
+      <CTABanner ctaContent={"Checkout Now"} link={checkout} />
       {/* cta banner :: end */}
     </div>
   );
